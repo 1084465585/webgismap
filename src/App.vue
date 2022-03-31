2df26ed1302ab97ce64b173a20c433a8a2a3902f
jinxin <1084465585@qq.com>
2022-03-31 09:40:57 +0800

    初始化项目结构

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <span>ssss</span>
+    
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'App',
+  components: {
+
+  }
+}
+</script>
+
+<style>
+html,body,#app {
+  position: relative;
+  width:100%;
+  height: 100%;
+  margin: 0;
+}
+</style>
